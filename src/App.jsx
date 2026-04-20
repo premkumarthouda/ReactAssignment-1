576b4f53e2a777c4a07d6f5a72c676b7b5179ab8
premkumarthouda <thoudapremkumar@gmail.com>
2026-04-20 19:41:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 
 import reactLogo from './assets/react.svg'
+import prempt from '../prempt.png'
 import './App.css'
 
 function App() {
 
   return (
-    <div className="cont1">
-    <div className='container'>
-      <img src={reactLogo} alt="React Logo" className='logo'/>
-      <h1 className='head-1'>Rules of JSX</h1>
-
-    </div>
-    <ul className='unorderList'>
-      <li>Must have enclosing JSX in root element</li>
-      <li>Close tag properly</li>
-      <li>Use className for css ,Js reserve class </li>
-      <li>use camelCase for attributes</li>
-      <li>use flower brackets for JSX expressions</li>
-    </ul>
-
-
+    <div className="container">
+      <img src={prempt} alt="Profile Loading..." className='Profile' />
+      <h1 className='name'>Prem Kumar</h1>
+      <p className='role'>Software Developer</p>
+      <p className='description'>At NxtWave’s CCBP 4.0 Academy, I’m currently learning full-stack development with a specialisation in 4.0 technologies. In love with the coding challenges and hands-on projects. With Industry’s first ever IRC 4.0, getting ready to take on new challenges in the tech world.</p>
+      <nav className='nav-conatiner'>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className='nav-link'><p>Twitter</p></a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className='nav-link'><p>Instagram </p></a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className='nav-link'><p>Linkdin</p></a>
+      </nav>
+ 
     </div>
   )
 }
